6990e03cff6436fccff80b3b9cfb63913120c1fb
Magesh <magesh.s@sedin.co.in>
2011-03-28 14:44:19 +0530

    Added restful-authentication, it works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+   # Restful-authentication
+    Dir.glob("./lib/*.{rb}").each { |file| require file }
+
+  include AuthenticatedSystem
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110328064406) do
+ActiveRecord::Schema.define(:version => 20110328084602) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,5 +19,19 @@ ActiveRecord::Schema.define(:version => 20110328064406) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
