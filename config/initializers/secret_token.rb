bce501d58f0588ee0b3bd1b13673d3eaf45003ed
Magesh <magesh.s@sedin.co.in>
2011-03-28 12:21:04 +0530

    created a simple blog using scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6a56fdbb0a0ae5a147d694a193b3aeb084d4891834d2b1aeac828cc5c0d1f4c1b536d4e1f6c44b87391c542600327b516c9e2dcb83eb2d65ab4cd17436b17d39'
